d0b680889db7412cc28632882390bdf097f570ef
Emilio Depena <emiliosdep@gmail.com>
2020-10-06 16:14:25 -0400

    room/client

--- src/com/emisdep/Main.java
+++ src/com/emisdep/Main.java
@@ -4,5 +4,8 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        Room room = new Room();
+        room.setNeedsCleaning(false);
+        System.out.println(room.getNeedsCleaning());
     }
 }
